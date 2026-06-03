29c2623c567891179679a9dd09c75bbaafde139b
Sean McBride <brennsto@gmail.com>
2026-06-02 20:09:17 -0700

    wip

--- src/main.cpp
+++ src/main.cpp
@@ -1,114 +1,116 @@
-//SAA1099P midi controller
+// SAA1099P midi controller
 //
 
+#include "note.h"
+#include "saa.h"
 #include <Arduino.h>
 #include <MIDI.h>
-#include "saa.h"
-#include "note.h"
 
 #ifdef USB_MIDI
-//read through usb serial
-struct CustomBaud : public midi::DefaultSettings{
-    static const long BaudRate = 38400; // Baud rate for hairless
-    static const unsigned SysExMaxSize = 1024; // Accept SysEx messages up to 1024 bytes long.
-    static const bool UseRunningStatus = true; // My devices seem to be ok with it.
+// read through usb serial
+struct CustomBaud : public midi::DefaultSettings {
+  static const long BaudRate = 38400; // Baud rate for hairless
 };
 MIDI_CREATE_CUSTOM_INSTANCE(HardwareSerial, Serial, MIDI, CustomBaud);
 #else
-//hardware midi port
+// hardware midi port
 MIDI_CREATE_INSTANCE(HardwareSerial, Serial1, MIDI);
 #endif
 
 static unsigned long lastUpdate = 0;
 
-void setup(){
+void setup() {
 
-    //magic to get 8mhz clock on pin A5
-    //credit to: https://forum.arduino.cc/t/how-to-get-8mhz-on-a-gpio-of-a-nano-every/704106/4
-    // LUT0 is the D input, which is connected to the flipflop output
-        CCL.LUT0CTRLB = CCL_INSEL0_FEEDBACK_gc; // Input from sequencer
-        CCL.TRUTH0 = 1; // Invert the input
-        CCL.SEQCTRL0 = CCL_SEQSEL0_DFF_gc; // D flipflop
-        // The following line configures using the system clock, OUTEN, and ENABLE
-        CCL.LUT0CTRLA = CCL_OUTEN_bm | CCL_ENABLE_bm;
-        // LUT1 is the D flipflop G input, which is always high
-        CCL.TRUTH1 = 0xff; 
-        CCL.LUT1CTRLA = CCL_ENABLE_bm; // Turn on LUT1
-        CCL.CTRLA = 1; // Enable the CC
-    //end magic
+  // magic to get 8mhz clock on pin A5
+  // credit to:
+  // https://forum.arduino.cc/t/how-to-get-8mhz-on-a-gpio-of-a-nano-every/704106/4
+  //  LUT0 is the D input, which is connected to the flipflop output
+  CCL.LUT0CTRLB = CCL_INSEL0_FEEDBACK_gc; // Input from sequencer
+  CCL.TRUTH0 = 1;                         // Invert the input
+  CCL.SEQCTRL0 = CCL_SEQSEL0_DFF_gc;      // D flipflop
+  // The following line configures using the system clock, OUTEN, and ENABLE
+  CCL.LUT0CTRLA = CCL_OUTEN_bm | CCL_ENABLE_bm;
+  // LUT1 is the D flipflop G input, which is always high
+  CCL.TRUTH1 = 0xff;
+  CCL.LUT1CTRLA = CCL_ENABLE_bm; // Turn on LUT1
+  CCL.CTRLA = 1;                 // Enable the CC
+                                 // end magic
 
-    //init pins
-     pinMode(clockPin, OUTPUT);
-     pinMode(latchPin, OUTPUT);
-     pinMode(dataPin, OUTPUT);
-     pinMode(pinWR, OUTPUT);
-     pinMode(pinAO, OUTPUT);
+  // init pins
+  pinMode(LED_BUILTIN, OUTPUT);
+  pinMode(clockPin, OUTPUT);
+  pinMode(latchPin, OUTPUT);
+  pinMode(dataPin, OUTPUT);
+  pinMode(pinWR, OUTPUT);
+  pinMode(pinAO, OUTPUT);
 
- 	//Reset/Enable all the sound channels ; I think this is what the original code said???
- 	write_data(0x1C, 0x02);
-    write_data(0x1C, 0x00);
-    write_data(0x1C, 0x01);
+  // Reset/Enable all the sound channels ; I think this is what the original
+  // code said???
+  write_data(0x1C, 0x02);
+  write_data(0x1C, 0x00);
+  write_data(0x1C, 0x01);
 
- 	//Disable the noise channels
-    write_data(0x15, 0x00);
+  // Disable the noise channels
+  write_data(0x15, 0x00);
 
-    //Disable envelopes on Channels 2 and 5
-    write_data(0x18, 0x00);
-    write_data(0x19, 0x00);
+  // Disable envelopes on Channels 2 and 5
+  write_data(0x18, 0x00);
+  write_data(0x19, 0x00);
 
-    //envelope control test set triangle envelope 2 and 5
-    //write_data(0x18, 0x8A);
-    //write_data(0x19, 0x8A);
+  // envelope control test set triangle envelope 2 and 5
+  // write_data(0x18, 0x8A);
+  // write_data(0x19, 0x8A);
 
-    // Connect the handleNoteOn function to the library,
-    // so it is called upon reception of a NoteOn.
-    MIDI.setHandleNoteOn(handle_note_on);    // Put only the name of the function
+  // Connect the handleNoteOn function to the library,
+  // so it is called upon reception of a NoteOn.
+  MIDI.setHandleNoteOn(handle_note_on); // Put only the name of the function
 
-    // Do the same for NoteOffs
-    MIDI.setHandleNoteOff(handle_note_off);
+  // Do the same for NoteOffs
+  MIDI.setHandleNoteOff(handle_note_off);
 
-    // Initiate MIDI communications, listen to all channels
-    MIDI.begin(MIDI_CHANNEL_OMNI);
-    #ifdef USB_MIDI
-    Serial.begin(38400); //for midi over USB
-    #endif
-    
-    //startup noise
-    //start_note(3, 24, 64);
-    handle_note_on(007, 38, 100);
-    delay(32);
-    start_note(0, 48, 64);
-    delay(32);
-    start_note(1, 52, 64);
-    delay(32);
-    start_note(2, 55, 64);
-    delay(32);
-    start_note(3, 60, 64);
-    delay(32);
-    start_note(4, 64, 64);
-    delay(1028);
+#ifdef USB_MIDI
+  Serial.begin(38400);
+#endif
+  MIDI.begin(MIDI_CHANNEL_OMNI);
+  MIDI.turnThruOff();
 
-    stop_note(0);
-    stop_note(1);
-    stop_note(2);
-    stop_note(3);
-    stop_note(4);
-    stop_note(5);
+  // startup noise
+  start_note(3, 24, 64);
+  delay(32);
+  start_note(0, 48, 64);
+  delay(32);
+  start_note(1, 52, 64);
+  delay(32);
+  start_note(2, 55, 64);
+  delay(32);
+  start_note(3, 60, 64);
+  delay(32);
+  start_note(4, 64, 64);
+  delay(1028);
+
+  stop_note(0);
+  stop_note(1);
+  stop_note(2);
+  stop_note(3);
+  stop_note(4);
+  stop_note(5);
+
+  while (Serial.available()) Serial.read();
 }
 
-void loop(){
+void loop() {
 
- MIDI.read();
- unsigned long now = millis();
+  MIDI.read();
+  unsigned long now = millis();
 
-//10ms ADSR check
- if ( (now - lastUpdate) > 10 ) {
-     lastUpdate += 10;
+  // 10ms ADSR check
+  if ((now - lastUpdate) > 10) {
+    lastUpdate += 10;
 
-     for (int i = 0; i < 6; i++){
-         proc_attack(i);
-         proc_decay(i);
-         proc_release(i);
-        }
+    for (int i = 0; i < 6; i++) {
+      proc_attack(i);
+      proc_decay(i);
+      proc_release(i);
     }
+  }
 }
--- src/note.cpp
+++ src/note.cpp
@@ -6,201 +6,216 @@ struct Status output_status[6];
 const int release_rate = 12;
 const int attack_rate = 4;
 const int decay_rate = 4;
+static const byte overtone_offsets[] = {12, 18, 24};
 
-//equal temperment approximation of real notes
+// equal temperment approximation of real notes
 const byte note_adr[] = {5, 32, 60, 85, 110, 132, 153, 173, 192, 210, 227, 243};
 
-//note addressing adapted from https://github.com/Bobcatmodder/SAATunes
-void start_note (byte chan, byte note, byte volume) {
+// note addressing adapted from https://github.com/Bobcatmodder/SAATunes
+void start_note(byte chan, byte note, byte volume) {
 
-    const byte octave_adr[] = {0x10, 0x11, 0x12}; //The 3 octave addresses (was 10, 11, 12)
-    const byte channel_adr[] = {0x08, 0x09, 0x0A, 0x0B, 0x0C, 0x0D}; //Addresses for the channel frequencies
-    byte addressOut;
-    byte dataOut;
+  const byte octave_adr[] = {0x10, 0x11,
+                             0x12}; // The 3 octave addresses (was 10, 11, 12)
+  const byte channel_adr[] = {
+      0x08, 0x09, 0x0A,
+      0x0B, 0x0C, 0x0D}; // Addresses for the channel frequencies
+  byte addressOut;
+  byte dataOut;
 
-    //Shift the note down by 1, since MIDI octaves start at C, but octaves on the SAA1099 start at B
-    note += 1;
+  // Shift the note down by 1, since MIDI octaves start at C, but octaves on the
+  // SAA1099 start at B
+  note += 1;
 
-    byte octave = (note / 12) - 1; //Some fancy math to get the correct octave
-    byte noteVal = note - ((octave + 1) * 12); //More fancy math to get the correct note
+  byte octave = (note / 12) - 1; // Some fancy math to get the correct octave
+  byte noteVal =
+      note - ((octave + 1) * 12); // More fancy math to get the correct note
 
-    //skip octave setting if same as before
-    if(output_status[chan].prevOctaves != octave){
-        //if new octave set it here
-        output_status[chan].prevOctaves = octave; //Set this variable so we can remember /next/ time what octave was /last/ played on this channel
-        //set octave address
-        addressOut = octave_adr[chan / 2];
-        //set octave data
-        if (chan == 0 || chan == 2 || chan == 4) {
-            dataOut = octave | (output_status[chan + 1].prevOctaves << 4); //Do fancy math so that we don't overwrite what's already on the register, except in the area we want to.
-        }
-        if (chan == 1 || chan == 3 || chan == 5) {
-            dataOut = (octave << 4) | output_status[chan - 1].prevOctaves ; //Do fancy math so that we don't overwrite what's already on the register, except in the area we want to.
-        }
-        write_data(addressOut, dataOut); //write octave
+  // skip octave setting if same as before
+  if (output_status[chan].prevOctaves != octave) {
+    // if new octave set it here
+    output_status[chan].prevOctaves =
+        octave; // Set this variable so we can remember /next/ time what octave
+                // was /last/ played on this channel
+    // set octave address
+    addressOut = octave_adr[chan / 2];
+    // set octave data
+    if (chan == 0 || chan == 2 || chan == 4) {
+      dataOut =
+          octave |
+          (output_status[chan + 1].prevOctaves
+           << 4); // Do fancy math so that we don't overwrite what's already on
+                  // the register, except in the area we want to.
     }
-    
-    //Note addressing and playing code
-    //Set address to the channel's address
-    addressOut = channel_adr[chan];
-
-    //set note data and write address + data
-    dataOut = note_adr[noteVal];
-    write_data(addressOut, dataOut);
-
-    //Setting volume to match velocity - decay and release handled elsewhere
-    //Set the Address to the volume channel
-    addressOut = chan;
-
-    //code to treat velocity as volume, reduce upper bound so that attack doesn't take us past max
-    byte vol = (volume / 8);
-    if (vol <= 0){
-        vol = 1;
+    if (chan == 1 || chan == 3 || chan == 5) {
+      dataOut = (octave << 4) |
+                output_status[chan - 1]
+                    .prevOctaves; // Do fancy math so that we don't overwrite
+                                  // what's already on the register, except in
+                                  // the area we want to.
     }
-    if (vol >= 11){
-        vol = 11;
-    }
+    write_data(addressOut, dataOut); // write octave
+  }
 
-    output_status[chan].lastVolume = vol;
+  // Note addressing and playing code
+  // Set address to the channel's address
+  addressOut = channel_adr[chan];
 
-	dataOut = (vol << 4) | vol;
+  // set note data and write address + data
+  dataOut = note_adr[noteVal];
+  write_data(addressOut, dataOut);
 
-    write_data(addressOut, dataOut);
+  // Setting volume to match velocity - decay and release handled elsewhere
+  // Set the Address to the volume channel
+  addressOut = chan;
 
-    output_status[chan].sinceOff = 0;
-    output_status[chan].sinceOn = 0;
+  // code to treat velocity as volume, reduce upper bound so that attack doesn't
+  // take us past max
+  byte vol = (volume / 8);
+  if (vol <= 0) {
+    vol = 1;
+  }
+  if (vol >= 11) {
+    vol = 11;
+  }
+
+  output_status[chan].lastVolume = vol;
+
+  dataOut = (vol << 4) | vol;
+
+  write_data(addressOut, dataOut);
+
+  output_status[chan].sinceOff = 0;
+  output_status[chan].sinceOn = 0;
+  output_status[chan].attackCount = 0;
 }
 
-void stop_note (byte chan) {
-    byte addressOut = chan;
-    write_data(addressOut, B00000000);
-    output_status[chan].channelActive = false;
-    output_status[chan].sinceOff = 0;
-    output_status[chan].attackCount = 0;
+void stop_note(byte chan) {
+  byte addressOut = chan;
+  write_data(addressOut, B00000000);
+  output_status[chan].channelActive = false;
+  output_status[chan].sinceOff = 0;
+  output_status[chan].attackCount = 0;
 }
 
 void handle_note_on(byte channel, byte pitch, byte velocity) {
-    
-    short int channel_out = get_channel_out();
+  if (channel == 10) {
+    // percussion handling goes here
+    return;
+  }
 
+  short int channel_out = get_channel_out();
+
+  output_status[channel_out].channelActive = true;
+  output_status[channel_out].keyOn = true;
+  output_status[channel_out].currentPitch = pitch;
+  start_note(channel_out, pitch, velocity);
+
+  for (byte j = 0; j < sizeof(overtone_offsets); j++) {
+    if (!is_channel_free())
+      break;
+    channel_out = get_channel_out();
     output_status[channel_out].channelActive = true;
     output_status[channel_out].keyOn = true;
-    output_status[channel_out].currentPitch = pitch;
-
-    start_note(channel_out, pitch, velocity);
-
-    //check if another channel is free, if so play octave
-    if (is_channel_free()){
-        channel_out = get_channel_out();
-        output_status[channel_out].channelActive = true;
-        output_status[channel_out].keyOn = true;
-        output_status[channel_out].currentPitch = (pitch+12);
-        start_note(channel_out, (pitch + 12), velocity);
-    }
-    //now add overtone
-    if (is_channel_free()){
-        channel_out = get_channel_out();
-        output_status[channel_out].channelActive = true;
-        output_status[channel_out].keyOn = true;
-        output_status[channel_out].currentPitch = (pitch+18);
-        start_note(channel_out, (pitch + 18), velocity);
-    }
-    //now add another octave
-    if (is_channel_free()){
-        channel_out = get_channel_out();
-        output_status[channel_out].channelActive = true;
-        output_status[channel_out].keyOn = true;
-        output_status[channel_out].currentPitch = (pitch+24);
-        start_note(channel_out, (pitch + 24), velocity);
-    }
+    output_status[channel_out].currentPitch = pitch + overtone_offsets[j];
+    start_note(channel_out, pitch + overtone_offsets[j], velocity);
+  }
 }
 
 void handle_note_off(byte channel, byte pitch, byte velocity) {
-
-    for (int i = 0; i < 6; i++){
-        if ((output_status[i].currentPitch == pitch) && (output_status[i].keyOn == true)){
-            output_status[i].keyOn = false;
-        }
-        if ((output_status[i].currentPitch == pitch + 12) && (output_status[i].keyOn == true)){
-            output_status[i].keyOn = false;
-        }
-        if ((output_status[i].currentPitch == pitch + 18) && (output_status[i].keyOn == true)){
-            output_status[i].keyOn = false;
-        }
-        if ((output_status[i].currentPitch == pitch + 24) && (output_status[i].keyOn == true)){
-            output_status[i].keyOn = false;
-        }
+  for (int i = 0; i < 6; i++) {
+    if (output_status[i].currentPitch == pitch && output_status[i].keyOn) {
+      output_status[i].keyOn = false;
     }
-}
-
-short int get_channel_out(){
-
-    for (int i = 0; i < 6; i++){
-        if (output_status[i].channelActive == false){
-            return i;
-        }
+    for (byte j = 0; j < sizeof(overtone_offsets); j++) {
+      if (output_status[i].currentPitch == pitch + overtone_offsets[j] &&
+          output_status[i].keyOn) {
+        output_status[i].keyOn = false;
+      }
     }
-    return random(0,5);
+  }
 }
 
-bool is_channel_free(){
-    for (int i = 0; i < 6; i++){
-        if (output_status[i].channelActive == false){
-            return 1;
-        }
+short int get_channel_out() {
+
+  for (int i = 0; i < 6; i++) {
+    if (output_status[i].channelActive == false) {
+      return i;
     }
-    return 0;
+  }
+  int quietest = 0;
+  for (int i = 1; i < 6; i++) {
+    if (output_status[i].lastVolume < output_status[quietest].lastVolume)
+      quietest = i;
+  }
+  stop_note(quietest);
+  return quietest;
 }
 
-void proc_attack(short int i){
-    //ATTACK PROCESSING
-        if (output_status[i].channelActive == true && output_status[i].attackCount < 4){
-
-            output_status[i].sinceOn++;
-
-            if (output_status[i].sinceOn >= attack_rate){
-                output_status[i].lastVolume++;
-                output_status[i].attackCount++;
-                byte dataOut = (output_status[i].lastVolume << 4) | output_status[i].lastVolume; //write new volume
-                write_data(i, dataOut);
-                output_status[i].sinceOn = 0;
-            }
-        }
-}
-
-void proc_decay(short int i){
-    //decay PROCESSING - sustain is set by upper bound
-        if (output_status[i].channelActive == true && output_status[i].attackCount >= 4 && output_status[i].attackCount < 8 ){ //last number sets sustain
-
-            output_status[i].sinceOn++;
-
-            if (output_status[i].sinceOn >= decay_rate){
-                output_status[i].lastVolume--;
-                output_status[i].attackCount++;
-                byte dataOut = (output_status[i].lastVolume << 4) | output_status[i].lastVolume; //write new volume
-                write_data(i, dataOut);
-                output_status[i].sinceOn = 0;
-            }
-        }
-}
-
-void proc_release(short int i){
-    //release PROCESSING
-    //if key is off but sound is playing we are still in release
-    if (output_status[i].keyOn == false && output_status[i].channelActive == true){
-     output_status[i].sinceOff++;
-
-     if (output_status[i].sinceOff >= release_rate){
-         output_status[i].lastVolume--;
-
-         if (output_status[i].lastVolume <= 0){ //if release takes us to 0 stop note
-             stop_note(i);
-            }
-            else{
-                byte dataOut = (output_status[i].lastVolume << 4) | output_status[i].lastVolume; //write new volume
-                write_data(i, dataOut);
-                output_status[i].sinceOff = 0;
-            }
-        }
+bool is_channel_free() {
+  for (int i = 0; i < 6; i++) {
+    if (output_status[i].channelActive == false) {
+      return 1;
     }
+  }
+  return 0;
+}
+
+void proc_attack(short int i) {
+  // ATTACK PROCESSING
+  if (output_status[i].channelActive == true &&
+      output_status[i].attackCount < 4) {
+
+    output_status[i].sinceOn++;
+
+    if (output_status[i].sinceOn >= attack_rate) {
+      output_status[i].lastVolume++;
+      output_status[i].attackCount++;
+      byte dataOut = (output_status[i].lastVolume << 4) |
+                     output_status[i].lastVolume; // write new volume
+      write_data(i, dataOut);
+      output_status[i].sinceOn = 0;
+    }
+  }
+}
+
+void proc_decay(short int i) {
+  // decay PROCESSING - sustain is set by upper bound
+  if (output_status[i].channelActive == true &&
+      output_status[i].attackCount >= 4 &&
+      output_status[i].attackCount < 8) { // last number sets sustain
+
+    output_status[i].sinceOn++;
+
+    if (output_status[i].sinceOn >= decay_rate) {
+      output_status[i].lastVolume--;
+      output_status[i].attackCount++;
+      byte dataOut = (output_status[i].lastVolume << 4) |
+                     output_status[i].lastVolume; // write new volume
+      write_data(i, dataOut);
+      output_status[i].sinceOn = 0;
+    }
+  }
+}
+
+void proc_release(short int i) {
+  // release PROCESSING
+  // if key is off but sound is playing we are still in release
+  if (output_status[i].keyOn == false &&
+      output_status[i].channelActive == true) {
+    output_status[i].sinceOff++;
+
+    if (output_status[i].sinceOff >= release_rate) {
+      output_status[i].lastVolume--;
+
+      if (output_status[i].lastVolume <=
+          0) { // if release takes us to 0 stop note
+        stop_note(i);
+      } else {
+        byte dataOut = (output_status[i].lastVolume << 4) |
+                       output_status[i].lastVolume; // write new volume
+        write_data(i, dataOut);
+        output_status[i].sinceOff = 0;
+      }
+    }
+  }
 }
